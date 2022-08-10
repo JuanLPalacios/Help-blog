6fb4dd07997d53353d63b69e19b3cd433688aec8
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-10 16:58:01 -0500

    Add user's most_recent_posts method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
 second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
-(1..5).each do |i|
+(1..4).each do |i|
     Post.create(author: first_user, title: "Hello ##{i}", text: "This is my post #{i}")
 end
 first_post = Post.first
-(1..7).each do |i|
+(1..6).each do |i|
     Comment.create(post: first_post, author: second_user, text: "comment #{i}" )
 end
