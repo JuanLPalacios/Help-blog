ff04f1d4a7db918b54a03b9dc1a444c6ca24e43a
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-10 17:20:21 -0500

    Add user's most_recent_posts method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
-    has_many :comments
-    has_many :likes
-    belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User'
 
-    def update_posts_counter
-        author.increment!(:posts_counter)
-    end
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 
-    def most_recent_comments
-        comments.order(created_at: :desc).limit(5)
-    end
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
