ca180da497e10c3e4223e2c2a699afa90b38208f
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-10 18:10:06 -0500

    Set active records callbacks

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,16 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User'
-
-  def update_posts_counter
-    author.increment!(:posts_counter)
-  end
+  
+  after_create :update_post_counter
 
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end
