9151c0901926eba85a1b6f46cceddcfe86e6f209
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-11 04:22:39 -0500

    Add user model unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: { allow_blank: false, message: 'must not be blank.' }
-  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
