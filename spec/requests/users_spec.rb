4dbf0644c8e2c85e0b6bb5f8d80d8bf1fb1e0c19
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-15 18:45:47 -0500

    Add users requests spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
+  describe 'GET /users' do
+    before(:example) do
+      get '/users'
+    end
+    
+    it 'response status was correct.' do
       expect(response).to have_http_status(:success)
+    end
+    
+    it 'correct template was rendered.' do
+      expect(response).to render_template('index')
+    end
+    
+    it 'the response body includes correct placeholder text.' do
+      expect(response.body).to include('A list of ussers')
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
+  describe 'GET /users/1' do
+    before(:example) do
+      get '/users/1'
+    end
+
+    it 'response status was correct.' do
       expect(response).to have_http_status(:success)
+    end
+    
+    it 'correct template was rendered.' do
+      expect(response).to render_template('show')
+    end
+    
+    it 'the response body includes correct placeholder text.' do
+      expect(response.body).to include('User details')
     end
   end
 end
