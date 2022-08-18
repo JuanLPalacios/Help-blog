3add02a62224f6fc8354686ef50fe0ca3990d88b
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-17 21:23:44 -0500

    Add posts#index view basic structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    params['page'] = 1 if params['page'].nil? || ((params['page'].to_i < 1))
+    @user = User.find(params['user_id'].to_i)
+    @posts = @user.posts.order(id: :desc).limit(2).offset((params['page'].to_i - 1) * 2)
+    @page = params['page'].to_i
+    @n_pages = (@user.posts_counter.to_f / 2).ceil
+  end
 
   def show; end
 end
