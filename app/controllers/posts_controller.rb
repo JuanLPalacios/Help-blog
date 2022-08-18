e53b8ff938ae6bf12617e25f3194019be1d0dea4
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-18 18:36:18 -0500

    Add new coment form styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params['id'].to_i)
     @user = User.find(params['user_id'].to_i)
+    @current_user = current_user
   end
 
   private
