fd0f4c27d8505de077aee69a51b51ccdee7b722e
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-23 23:03:18 +0300

    Test: post section 2

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -13,42 +13,47 @@ RSpec.describe 'Posts', type: :request do
       expect(page).to have_content(@user.name)
     end
     it 'I can see the number of posts the user has written.' do
-      expect(page).to have_content("Number of Posts: #{@user.postCount}")
+      expect(page).to have_content("Number of posts: #{@user.posts_counter}")
     end
     it "I can see a post's title." do
-      @user.posts.each do |post|
+      @user.posts.order(id: :desc).each_with_index do |post,index|
         expect(page).to have_content(post.title)
+        visit user_posts_path(1,page: (index/2)+2) if index%2==1
       end
     end
     it "I can see some of the post's body." do
-      @user.posts.each do |post|
+      @user.posts.order(id: :desc).each_with_index do |post,index|
         expect(page).to have_content(post.text[0..100])
+        visit user_posts_path(1,page: (index/2)+2) if index%2==1
       end
     end
     it 'I can see the first comments on a post.' do
-      @user.posts.each do |post|
+      @user.posts.order(id: :desc).each_with_index do |post,index|
         first_comment = post.comments.order(created_at: :desc).first
         expect(page).to have_content(first_comment.text) unless first_comment.nil?
+        visit user_posts_path(1,page: (index/2)+2) if index%2==1
       end
     end
     it 'I can see how many comments a post has.' do
-      @user.posts.each do |post|
-        expect(page).to have_content("comments: #{post.commentsCount}")
+      @user.posts.order(id: :desc).each_with_index do |post,index|
+        expect(page).to have_content("Comments: #{post.comments_counter}")
+        visit user_posts_path(1,page: (index/2)+2) if index%2==1
       end
     end
     it 'I can see how many likes a post has.' do
-      @user.posts.each do |post|
-        expect(page).to have_content("Likes: #{post.likesCount}")
+      @user.posts.order(id: :desc).each_with_index do |post,index|
+        expect(page).to have_content("Likes: #{post.likes_counter}")
+        visit user_posts_path(1,page: (index/2)+2) if index%2==1
       end
     end
     it 'I can see a section for pagination if there are more posts than fit on the view.' do
-      expect(page).to have_content('Pagination')
+      expect(page).to have_xpath("//section[@id='pagination']")
     end
     it "When I click on a post, it redirects me to that post's show page." do
-      @user.posts.each do |post|
+      @user.posts.order(id: :desc).each_with_index do |post,index|
+        visit user_posts_path(1,page: (index/2)+1)
         find(:xpath, "//a[@href='/users/#{@user.id}/posts/#{post.id}']").click
         expect(page).to have_current_path("/users/#{@user.id}/posts/#{post.id}")
-        visit user_posts_path(@user)
       end
     end
   end
@@ -57,7 +62,7 @@ RSpec.describe 'Posts', type: :request do
     before(:example) { visit users_path }
     before(:all) do
       @post = Post.find 1
-      @user = @post.user
+      @user = @post.author
     end
     it "I can see the post's title." do
     end
