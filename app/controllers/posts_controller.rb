e574d63f6175c6abea2c79d9e3fadda23efbc4e1
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-23 12:26:56 -0500

    Add users integation tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     params['page'] = 1 if params['page'].nil? || ((params['page'].to_i < 1))
     @user = User.find(params['user_id'].to_i)
     @posts = @user.posts.order(id: :desc).limit(2).offset((params['page'].to_i - 1) * 2)
+      .includes(:comments, comments: [:author])
     @page = params['page'].to_i
     @n_pages = (@user.posts_counter.to_f / 2).ceil
   end
