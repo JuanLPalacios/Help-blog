b411aed4e7b219263e62d0f5b86a96f56cf4be33
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-26 18:27:03 -0500

    Update seed to include confirmed user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,9 @@ class PostsController < ApplicationController
   def index
     params['page'] = 1 if params['page'].nil? || ((params['page'].to_i < 1))
     @user = User.find(params['user_id'].to_i)
-    @posts = @user
+    @posts = @user.posts
       .includes(:comments, comments: [:author])
-      .posts.order(id: :desc).limit(2).offset((params['page'].to_i - 1) * 2)
+      .order(id: :desc).limit(2).offset((params['page'].to_i - 1) * 2)
     @page = params['page'].to_i
     @n_pages = (@user.posts_counter.to_f / 2).ceil
   end
