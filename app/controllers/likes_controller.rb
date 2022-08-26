9097780e99fab7f6c3eecc51b5e814462a6a1ebd
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-08-26 18:01:34 -0500

    Add json response to likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,9 @@
 class LikesController < ApplicationController
   def create
-    Like.create(author: current_user, post_id: params['post_id'].to_i)
-    redirect_to user_post_path(params['user_id'], params['post_id'])
+    @like Like.create(author: current_user, post_id: params['post_id'].to_i)
+    respond_to do |format|
+      format.html { redirect_to user_post_path(params['user_id'], params['post_id']) }
+      format.json { render :show, status: :created, location: @like }
+    end
   end
 end
